7ba40fa4c5a66e7ce546aa2c7dc719d5eb3d948d
Kornelius Kalnbach <murphy@rubychan.de>
2015-01-14 20:09:19 +0100

    update cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+# config valid only for Capistrano 3
+lock '3.3.5'
 
 set :application, 'blokk'
 set :repo_url, 'git@github.com:korny/blokk.git'
